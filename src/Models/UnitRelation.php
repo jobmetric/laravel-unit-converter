b46ec3223ee2e796b5db807f9695b7297d0352b9
majidmohammadian <majeedmohammadian@gmail.com>
2026-02-06 00:11:56 +0330

    changed model UnitRelation

--- src/Models/UnitRelation.php
+++ src/Models/UnitRelation.php
@@ -2,73 +2,128 @@
 
 namespace JobMetric\UnitConverter\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use JobMetric\PackageCore\Traits\HasMorphResourceAttributes;
 
 /**
- * JobMetric\UnitConverter\Models\UnitRelation
+ * Class UnitRelation
  *
- * @property mixed unit_id
- * @property mixed unitable_type
- * @property mixed unitable_id
- * @property mixed type
- * @property mixed value
- * @property mixed created_at
+ * Represents a polymorphic relation between a unit and any Eloquent model
+ * that can use units. This allows attaching a unit to any entity (Product, Order, etc.)
+ * while also storing the unit type and the value in that unit.
  *
- * @property Unit unit
- * @property mixed unitable
- * @property mixed unitable_resource
+ * @package JobMetric\UnitConverter
  *
- * @method static Builder ofType(string $collection)
+ * @property int $unit_id            The attached unit identifier.
+ * @property string $unitable_type   The class name of the related model.
+ * @property int $unitable_id        The ID of the related model instance.
+ * @property string $type            The unit type (weight, length, volume, etc.).
+ * @property float $value            The value stored in the attached unit.
+ * @property Carbon $created_at      The timestamp when this relation was created.
+ *
+ * @property-read Unit $unit
+ * @property-read Model|MorphTo $unitable
+ * @property-read mixed $unitable_resource
+ *
+ * @method static Builder|UnitRelation whereUnitId(int $unit_id)
+ * @method static Builder|UnitRelation whereUnitableType(string $unitable_type)
+ * @method static Builder|UnitRelation whereUnitableId(int $unitable_id)
+ * @method static Builder|UnitRelation whereType(string $type)
+ * @method static Builder|UnitRelation whereValue(float $value)
+ * @method static Builder|UnitRelation ofType(string $type)
  */
 class UnitRelation extends Pivot
 {
     use HasFactory, HasMorphResourceAttributes;
 
-    const UPDATED_AT = null;
+    /**
+     * This table does not have Laravel's updated_at column.
+     *
+     * @var bool
+     */
+    public const UPDATED_AT = null;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'unit_id',
         'unitable_type',
         'unitable_id',
         'type',
-        'value'
+        'value',
+        'created_at',
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
      * @var array<string, string>
      */
     protected $casts = [
-        'unit_id' => 'integer',
+        'unit_id'       => 'integer',
         'unitable_type' => 'string',
-        'unitable_id' => 'integer',
-        'type' => 'string',
-        'value' => 'float',
+        'unitable_id'   => 'integer',
+        'type'          => 'string',
+        'value'         => 'float',
+        'created_at'    => 'datetime',
     ];
 
-    public function getTable()
+    /**
+     * Override the table name using config.
+     *
+     * @return string
+     */
+    public function getTable(): string
     {
         return config('unit.tables.unit_relation', parent::getTable());
     }
 
+    /**
+     * Initialize model events.
+     *
+     * @return void
+     */
+    protected static function booted(): void
+    {
+        // Ensure created_at is set by application layer as well (DB has default too).
+        static::creating(function (self $relation): void {
+            if (empty($relation->created_at)) {
+                $relation->created_at = now();
+            }
+        });
+    }
+
+    /**
+     * Get the related unit.
+     *
+     * @return BelongsTo
+     */
     public function unit(): BelongsTo
     {
         return $this->belongsTo(Unit::class);
     }
 
+    /**
+     * Get the related model (polymorphic).
+     *
+     * @return MorphTo
+     */
     public function unitable(): MorphTo
     {
         return $this->morphTo();
     }
 
     /**
-     * Scope a query to only include categories of a given type.
+     * Scope a query to only include relations of a given unit type.
      *
      * @param Builder $query
      * @param string $type
