271286c6bc32fba57daca8d313115ba2561774b8
majidmohammadian <majeedmohammadian@gmail.com>
2026-02-08 03:07:38 +0330

    added some unit in UnitTypeEnum

--- src/Enums/UnitTypeEnum.php
+++ src/Enums/UnitTypeEnum.php
@@ -30,34 +30,56 @@ use JobMetric\PackageCore\Enums\EnumMacros;
  * @method static VOLUMETRIC_FLOW()
  * @method static ELECTRIC_CURRENT()
  * @method static HEAT_TRANSFER_COEFFICIENT()
+ * @method static LUMINOSITY()
+ * @method static ELECTRIC_VOLTAGE()
+ * @method static ELECTRIC_RESISTANCE()
+ * @method static ELECTRIC_CAPACITANCE()
+ * @method static ELECTRIC_INDUCTANCE()
+ * @method static MAGNETIC_FLUX()
+ * @method static RADIATION()
+ * @method static FUEL_CONSUMPTION()
+ * @method static VISCOSITY()
+ * @method static CONCENTRATION()
+ * @method static COOKING()
  */
 enum UnitTypeEnum: string
 {
     use EnumMacros;
 
-    case WEIGHT = "weight";                                                 /* واحد های وزن */
-    case LENGTH = "length";                                                 /* واحد های طول */
-    case CURRENCY = "currency";                                             /* واحد های ارز */
-    case NUMBER = "number";                                                 /* واحد های تعداد */
-    case CRYPTO = "crypto";                                                 /* واحد های رمز ارز */
-    case VOLUME = "volume";                                                 /* واحد های حجم */
-    case TEMPERATURE = "temperature";                                       /* واحد های دما */
-    case AREA = "area";                                                     /* واحد های مساحت */
-    case PRESSURE = "pressure";                                             /* واحد های فشار */
-    case SPEED = "speed";                                                   /* واحد های سرعت */
-    case FORCE = "force";                                                   /* واحد های نیرو */
-    case TIME = "time";                                                     /* واحد های زمان */
-    case TORQUE = "torque";                                                 /* واحد های گشتاور */
-    case ENERGY = "energy";                                                 /* واحد های انرژی */
-    case FREQUENCY = "frequency";                                           /* واحد های فرکانس */
-    case POWER = "power";                                                   /* واحد های توان */
-    case ACCELERATION = "acceleration";                                     /* واحد های شتاب */
-    case DATA_TRANSFER = "data_transfer";                                   /* واحد های انتقال داده */
-    case DATA_STORAGE = "data_storage";                                     /* واحد های ذخیره سازی داده */
-    case ANGLE = "angle";                                                   /* واحد های زاویه */
-    case DENSITY = "density";                                               /* واحد های چگالی */
-    case MASS_FLOW = "mass_flow";                                           /* واحد های جریان جرم */
-    case VOLUMETRIC_FLOW = "volumetric_flow";                               /* واحد های جریان حجم */
-    case ELECTRIC_CURRENT = "electric_current";                             /* واحد های جریان الکتریکی */
-    case HEAT_TRANSFER_COEFFICIENT = "heat_transfer_coefficient";           /* واحد های ضریب انتقال حرارت */
+    case WEIGHT = "weight";                                                 /* Weight units */
+    case LENGTH = "length";                                                 /* Length units */
+    case CURRENCY = "currency";                                             /* Currency units */
+    case NUMBER = "number";                                                 /* Number units */
+    case CRYPTO = "crypto";                                                 /* Cryptocurrency units */
+    case VOLUME = "volume";                                                 /* Volume units */
+    case TEMPERATURE = "temperature";                                       /* Temperature units */
+    case AREA = "area";                                                     /* Area units */
+    case PRESSURE = "pressure";                                             /* Pressure units */
+    case SPEED = "speed";                                                   /* Speed units */
+    case FORCE = "force";                                                   /* Force units */
+    case TIME = "time";                                                     /* Time units */
+    case TORQUE = "torque";                                                 /* Torque units */
+    case ENERGY = "energy";                                                 /* Energy units */
+    case FREQUENCY = "frequency";                                           /* Frequency units */
+    case POWER = "power";                                                   /* Power units */
+    case ACCELERATION = "acceleration";                                     /* Acceleration units */
+    case DATA_TRANSFER = "data_transfer";                                   /* Data transfer units */
+    case DATA_STORAGE = "data_storage";                                     /* Data storage units */
+    case ANGLE = "angle";                                                   /* Angle units */
+    case DENSITY = "density";                                               /* Density units */
+    case MASS_FLOW = "mass_flow";                                           /* Mass flow units */
+    case VOLUMETRIC_FLOW = "volumetric_flow";                               /* Volumetric flow units */
+    case ELECTRIC_CURRENT = "electric_current";                             /* Electric current units */
+    case HEAT_TRANSFER_COEFFICIENT = "heat_transfer_coefficient";           /* Heat transfer coefficient units */
+    case LUMINOSITY = "luminosity";                                         /* Luminosity units (lumen, candela, lux) */
+    case ELECTRIC_VOLTAGE = "electric_voltage";                             /* Electric voltage units (volt, millivolt, kilovolt) */
+    case ELECTRIC_RESISTANCE = "electric_resistance";                       /* Electric resistance units (ohm, kiloohm, megaohm) */
+    case ELECTRIC_CAPACITANCE = "electric_capacitance";                     /* Electric capacitance units (farad, microfarad) */
+    case ELECTRIC_INDUCTANCE = "electric_inductance";                       /* Electric inductance units (henry) */
+    case MAGNETIC_FLUX = "magnetic_flux";                                   /* Magnetic flux units (weber, tesla) */
+    case RADIATION = "radiation";                                           /* Radiation units (becquerel, sievert, gray) */
+    case FUEL_CONSUMPTION = "fuel_consumption";                             /* Fuel consumption units (L/km, mpg) */
+    case VISCOSITY = "viscosity";                                           /* Viscosity units (poise, stokes) */
+    case CONCENTRATION = "concentration";                                   /* Concentration units (mol/L, ppm) */
+    case COOKING = "cooking";                                               /* Cooking units (cup, tablespoon, teaspoon) */
 }
